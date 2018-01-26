f64945679c60ecf12a15e486e48f77d30b06efac
abraidotti <alessandro.braidotti@gmail.com>
2018-01-26 00:11:05 -0500

    finish up tutorial

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playerController : MonoBehaviour {
 
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
     private int count;
@@ -13,6 +16,8 @@ public class playerController : MonoBehaviour {
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     private void FixedUpdate()
@@ -31,6 +36,16 @@ public class playerController : MonoBehaviour {
         {
             other.gameObject.SetActive(false);
             count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count:" + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You win!";
         }
     }
 }
